445c8607df9b736050edde59ae35c99ec03c1bd3
Anik <135734362+anikalwayspro@users.noreply.github.com>
2023-08-30 17:21:19 +0530

    Update __main__.py
    Signed-off-by: Anik <135734362+anikalwayspro@users.noreply.github.com>

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -84,7 +84,7 @@ PM_START_TEXT = """
 buttons = [
     [
         InlineKeyboardButton(
-            text="ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ",
+            text="🧁 ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ 🧁",
             url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
         ),
     ],
