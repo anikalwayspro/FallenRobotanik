9e249a7ec09866d6408b9fade484c14007b1d40c
Anik <135734362+anikalwayspro@users.noreply.github.com>
2023-09-02 17:27:58 +0530

    Update memify.py
    Signed-off-by: Anik <135734362+anikalwayspro@users.noreply.github.com>

--- FallenRobot/modules/memify.py
+++ FallenRobot/modules/memify.py
@@ -77,7 +77,7 @@ async def drawText(image_path, text):
 
     else:
 
-        fnt = "./KaizuryuBot/resources/default.ttf"
+        fnt = "./FallenRobot/resources/default.ttf"
 
     m_font = ImageFont.truetype(fnt, int((70 / 640) * i_width))
 
